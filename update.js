25eb0bf3aaa2fff3122b578f31e9e3938875f600
RENZO <rbedini@miuandes.cl>
2021-05-16 18:27:05 -0400

    agrego de update

---
+++ update.js
@@ -0,0 +1,19 @@
+function tipValue(percent, billAmount){
+    return (percent*0.01)*billAmount
+}
+
+function update(billAmount,percent, model){
+    const newTip= tipValue(percent,billAmount)
+    const newTotal = 1*billAmount + newTip
+    return {
+        ...model,
+        total: newTotal,
+        billAmount: billAmount,
+        percent: percent,
+        tip:newTip
+    }
+}
+
+module.exports = {
+    update
+}
